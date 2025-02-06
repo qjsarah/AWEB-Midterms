faf3799ab084f2ba2e457e4a56d4af2d78dba9b7
qjsarah <165972652+qjsarah@users.noreply.github.com>
2025-02-06 20:51:19 +0800

    new project rest api

---
+++ http-client-prg/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpclientService } from './httpclient.service';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'http-client-prg';
+  httpusers: any;
+
+  constructor(private httpclientlist: HttpclientService){
+    this.httpclientlist.getUsersRemotely().subscribe((data:any)=>{
+      this.httpusers = data;
+    })
+  }
+}
